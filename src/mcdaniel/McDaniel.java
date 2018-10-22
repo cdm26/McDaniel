5c7a14b1afa85290f11b86918987ff2315e73894
cdm26@students.ptcollege.edu <>
2018-10-22 15:43:57 -0400

    First commit

---
+++ src/mcdaniel/McDaniel.java
@@ -0,0 +1,13 @@
+
+package mcdaniel;
+
+public class McDaniel {
+
+    public static void main(String[] args) {
+       McDaniel();
+    }
+    public static void McDaniel()
+    {
+        System.out.println("Chris McDaniel");
+    }
+}
